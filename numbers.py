989b41e2f46dbc5b31b9543d413ed658e417f4c5
zdenek-nemec <zdenek.nemec@artin.cz>
2019-09-30 20:51:14 +0200

    Numbers

---
+++ numbers.py
@@ -0,0 +1,13 @@
+number_integer = 7
+print(number_integer)
+print(type(number_integer))
+
+number_float = 7.0
+print(number_float)
+print(type(number_float))
+
+number_complex = 7+4j
+print(number_complex)
+print(type(number_complex))
+print(number_complex.real)
+print(number_complex.imag)
